a4193d06339862140eb755592036e5038134037e
Added through scyld-cloud-controller <sb95@podmt3-ce29fe7.pod.penguincomputing.com>
2022-04-07 01:24:06 +0000

    runSAC

--- sim_codes/slurm_run_analyze.sh
+++ sim_codes/slurm_run_analyze.sh
@@ -4,18 +4,18 @@ save_dest=~/Active_fluctuations/data/SAC_G1200_N100_R020
 run_code_home=~/Active_fluctuations/Active-noise-simulations/sim_codes
 analyze_code_home=~/Active_fluctuations/analysis_codes
 
-name=SAC
+name=SAC_exp
 seq=allA_seq.txt
 top=chromosome_top.txt
-finit=' ' #sample_snap_1200.npy
+finit=init_structure_G1200_N100_R020.npy #sample_snap_1200.npy
 ftype=type_table.csv
 
 G=1200
 kr=30.0
 kb=10.0
 Esoft=4.0
-R0=10.0
-nblocks=1020000
+R0=20.0
+nblocks=1000000
 blocksize=100
 dt=0.001
 #rm -r $path0
@@ -29,10 +29,10 @@ ii=0
 for T in 200.0; do
 mkdir $save_dest/T_$T
 
-for F in 0.0; do
+for F in 0.1 0.3 0.7 1.0 2.0 5.0; do
 mkdir $save_dest/T_$T/F_$F
 
-for Ta in 1.0; do
+for Ta in 0.01 0.1 1.0 10.0 50.0 100.0 1000.0; do
 mkdir $save_dest/T_$T/F_$F/Ta_$Ta
 
 # for kb in 10.0; do
@@ -59,16 +59,16 @@ cp $run_code_home/run_analyze.py $sim_home
 
 python_venv="#!/bin/bash -l
 source ~/venv/containers/openmm/bin/activate
-python3 run_sims.py -name $name -dt $dt -ftype $ftype -ftop $top  -fseq $seq -rep $replica -Ta $Ta -G $G -F $F -temp $T -kb $kb -Esoft $Esoft -nblocks $nblocks -blocksize $blocksize -R0 $R0 -kr $kr
+python3 run_sims.py -name $name -dt $dt -ftype $ftype -ftop $top  -fseq $seq -rep $replica -Ta $Ta -G $G -F $F -temp $T -kb $kb -Esoft $Esoft -nblocks $nblocks -blocksize $blocksize -R0 $R0 -kr $kr -finit $finit
 
-python3 run_analyze.py -s $save_dest/analysis/ -VCV #-gyr -RDP -MSD -bondlen -HiC -rep $replica
+python3 run_analyze.py -s $save_dest/analysis/ -VCV -gyr -RDP -MSD -HiC -rep $replica -bondlen
 "
 echo "$python_venv">"python_venv.sh"
 chmod u+x "python_venv.sh"
 
 slurm_file_content="#!/bin/bash -l
 
-#SBATCH --job-name=SAC_ipd
+#SBATCH --job-name=SAC
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --tasks-per-node=1
