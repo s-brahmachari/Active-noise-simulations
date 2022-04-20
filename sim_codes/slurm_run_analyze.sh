bb435d43a2bdcd607315a3d9989ba96bfef59879
Added through scyld-cloud-controller <sb95@podmt3-ce29fe7.pod.penguincomputing.com>
2022-04-20 21:00:02 +0000

    runs free

--- sim_codes/slurm_run_analyze.sh
+++ sim_codes/slurm_run_analyze.sh
@@ -1,41 +1,41 @@
 #!/bin/bash -l
 
-save_dest=~/Active_fluctuations/data/SAC_exp_G1200_N100_R020
+save_dest=~/Active_fluctuations/data/SAC_exp_G100_N100_free
 run_code_home=~/Active_fluctuations/Active-noise-simulations/sim_codes
 analyze_code_home=~/Active_fluctuations/analysis_codes
 
 name=SAC_exp
-seq=allA_seq.txt
-top=chromosome_top.txt
-finit=init_structure_G1200_N100_R020.npy #sample_snap_1200.npy
+seq=allA100_seq.txt
+top=chr_top.txt
+finit=init_structure_G100_N100_R020.npy #sample_snap_1200.npy
 ftype=type_table.csv
 
-G=1200
+G=100
 kr=30.0
 kb=10.0
 Esoft=4.0
-R0=20.0
-nblocks=1000000
+R0=0.0
+nblocks=800000
 blocksize=100
 dt=0.001
 #rm -r $path0
 
-#mkdir $save_dest
-#mkdir $save_dest/analysis
+mkdir $save_dest
+mkdir $save_dest/analysis
 
 ii=0
 # for Esoft in 0; do
 # mkdir $save_dest/Esoft_$Esoft
 
 for T in 200.0; do
-#mkdir $save_dest/T_$T
+mkdir $save_dest/T_$T
 
 #for F in 0.0 0.001 0.01; do
-for F in 0.7 1.0 2.0; do
+for F in 0.3 1.0 2.0; do
 #for F in 0.0; do
 mkdir $save_dest/T_$T/F_$F
 
-for Ta in 0.3 2.0 6.0; do
+for Ta in 0.1 1.0 10.0 100.0; do
 #for Ta in 0.01 0.1 1.0 10.0 50.0 100.0 1000.0; do
 #for Ta in 1.0; do 
 mkdir $save_dest/T_$T/F_$F/Ta_$Ta
@@ -43,7 +43,7 @@ mkdir $save_dest/T_$T/F_$F/Ta_$Ta
 # for kb in 10.0; do
 # mkdir $save_dest/T_$T/F_$F/Ta_$Ta/kb_$kb
 
-for replica in 1; do
+for replica in 1 2 3; do
  
 # sim_home=$save_dest/T_$T/F_$F/Ta_$Ta/kb_$kb
 sim_home=$save_dest/T_$T/F_$F/Ta_$Ta/replica_$replica
@@ -59,21 +59,21 @@ cp $run_code_home/ActivePolymer.py $sim_home
 
 cp $run_code_home/AnalyzeTrajectory.py $sim_home
 cp $run_code_home/run_analyze.py $sim_home
-cp $run_code_home/input_files/$ftype $sim_home
+#cp $run_code_home/input_files/$ftype $sim_home
 
 
 python_venv="#!/bin/bash -l
 source ~/venv/containers/openmm/bin/activate
 python3 run_sims.py -name $name -dt $dt -ftype $ftype -ftop $top  -fseq $seq -rep $replica -Ta $Ta -G $G -F $F -temp $T -kb $kb -Esoft $Esoft -nblocks $nblocks -blocksize $blocksize -R0 $R0 -kr $kr -finit $finit
 
-python3 run_analyze.py -s $save_dest/analysis/ -HiC -VCV -gyr -RDP -MSD -rep $replica -bondlen
+python3 run_analyze.py -s $save_dest/analysis/ -VCV -gyr -MSD -rep $replica
 "
 echo "$python_venv">"python_venv.sh"
 chmod u+x "python_venv.sh"
 
 slurm_file_content="#!/bin/bash -l
 
-#SBATCH --job-name=SAC
+#SBATCH --job-name=SAC_free
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --tasks-per-node=1
